1dfc673ddd9f9337fea7b6c7ea486657ba47a141
Caleb <Caleb@10.16.7.10>
2017-03-07 19:18:00 -0800

    I dont know

--- Just BeWeave Project/src/model/Admin.java
+++ Just BeWeave Project/src/model/Admin.java
@@ -2,9 +2,5 @@ package model;
 
 public class Admin extends AbstractUser {
 	
-	/*Peter needs to stop playing NFL football on his phone in class*/
-=======
 	/*Whats up guys*/
->>>>>>> branch 'master' of git@github.com:mmdahir/OperationTangerine.git
-
 }
