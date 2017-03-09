0615debdc2b37d927494d38b9c01923f3d46b3c9
John <John@192.168.0.17>
2017-03-09 13:18:20 -0800

    can register and login

--- Just BeWeave Project/src/view/LoginPane.java
+++ Just BeWeave Project/src/view/LoginPane.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -10,16 +13,48 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+import javafx.scene.text.Text; 
 
-public class LoginPane extends GridPane { {
+import model.AbstractUser;
+import model.Admin;
+import model.Date;
+import model.DataBase;
+import model.Event;
+import model.NonAdmin;
+import model.User;
+
+/**
+ * 
+ * @author John
+ *
+ */
+public class LoginPane extends GridPane {
+	
+	/** Data base used to access csv file. */
+	private DataBase myDataBase = new DataBase();
+	
+	/** Current User. */
+	private User myUser;
+	
+	/** Current User name. */
+	private String myName;
+	
+	/** Current User passowrd. */
+	private String myPass;
+	
+	{
 
 	this.setAlignment(Pos.TOP_CENTER);
 	this.setHgap(10);
 	this.setPadding(new Insets(20, 0, 0, 0));
 	this.setMinSize(250, 500);
+	
+	Text scenetitle = new Text("Login");
+	scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+	this.add(scenetitle, 0, 0, 2, 1);
 	
 	// user name text field
 	Label userName = new Label("User Name:");
@@ -37,28 +72,68 @@ public class LoginPane extends GridPane { {
 	
 	Button btn = new Button("Sign in");
 	HBox hbBtn = new HBox(10);
-	hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+	hbBtn.setAlignment(Pos.BOTTOM_LEFT);
 	hbBtn.getChildren().add(btn);
 	this.add(hbBtn, 1, 4);
+	
+	// user name/password invalid
+	final Text actiontarget = new Text();
+    this.add(actiontarget, 1, 6);
 	
 	// action for sign in button
     btn.setOnAction(new EventHandler<ActionEvent>() {
     	 
         @Override
         public void handle(ActionEvent e) {
+        	myName = userTextField.getText();
+        	myPass = pwBox.getText();
         	
-        	System.out.println("your user name: " + userTextField.getText());
-        	System.out.println("your password: " + pwBox.getText());
-        	getChildren().clear();
-        	if(userTextField.getText().equals("john")) {
-        		update(userTextField.getText(), true);
+        	// creates current user
+        	try {
+				myUser = myDataBase.verifyUser(myName, myPass);
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+        	
+        	// checks if user is valid and if admin
+        	if(myUser == null) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Incorrect login information");
         	}else{
-        		update(userTextField.getText(), false);
-        	}
+        		getChildren().clear();
+        		if(myUser.isAdmin()) {
+        			update(userTextField.getText(), true);
+        		}else {
+        			update(userTextField.getText(), false);
+        		}
+       		}
         		
         }
     });
+    
+    Button reg = new Button("Register");
+	hbBtn.getChildren().add(reg);
+    
+	reg.setOnAction(new EventHandler<ActionEvent>() {
+   	 
+        @Override
+        public void handle(ActionEvent e) {
+        	getChildren().clear();
+        	register();
+        }
+	});
+	
+    
 }
+	/**
+	 * Opens account window for admin and user.
+	 * @param theName the user name
+	 * @param admin whether or not user is admin
+	 */
     public void update(String theName, Boolean admin) {
     	
     	Text scenetitle = new Text(theName);
@@ -80,7 +155,7 @@ public class LoginPane extends GridPane { {
 	        	 
 	            @Override
 	            public void handle(ActionEvent e) {
-	            	
+	            	getChildren().clear();
 	            	
 	            }
 	        });
@@ -100,12 +175,68 @@ public class LoginPane extends GridPane { {
 	        	 
 	            @Override
 	            public void handle(ActionEvent e) {
-	            	
+	            	getChildren().clear();
 	            	
 	            }
 	        });
     	}
+    }
     
-    
+    /**
+     * Creates register window.
+     */
+    public void register() {
+    	Text scenetitle = new Text("Register");
+    	scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+    	this.add(scenetitle, 0, 0, 2, 1);
+    	
+    	// user name text field
+    	Label userName = new Label("User Name:");
+    	this.add(userName, 0, 1);
+
+    	TextField userTextField = new TextField();
+    	this.add(userTextField, 1, 1);
+    	
+    	// user name text field
+    	Label password = new Label("Password:");
+    	this.add(password, 0, 2);
+
+    	TextField passTextField = new TextField();
+    	this.add(passTextField, 1, 2);
+    	
+    	final Text actiontarget = new Text();
+        this.add(actiontarget, 1, 6);
+    	
+    	Button save = new Button("Submit");
+    	HBox hbSave = new HBox(10);
+    	hbSave.setAlignment(Pos.BOTTOM_RIGHT);
+    	hbSave.getChildren().add(save);
+    	this.add(hbSave, 1, 4);
+    	
+    	// action for sign in button
+        save.setOnAction(new EventHandler<ActionEvent>() {
+        	 
+            @Override
+            public void handle(ActionEvent e) {
+            	getChildren().clear();
+            	if(passTextField.getText() == "" || userTextField.getText() == "") {
+            		actiontarget.setFill(Color.FIREBRICK);
+            		actiontarget.setText("Invalid input");
+            	}else {
+            		myName = userTextField.getText();
+            		myPass = passTextField.getText();
+            		User newUser = new NonAdmin(userTextField.getText(), passTextField.getText(), false);
+            		try {
+						myDataBase.saveUser(newUser);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+            	}
+            	update(myName, false);
+               
+            }
+        });
+    	
     }
 }
