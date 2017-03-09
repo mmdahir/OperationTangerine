defde98e5203f756409191409f02b78206eee9ef
Peter <Peter@pc>
2017-03-09 02:34:17 -0800

    1

--- Just BeWeave Project/src/model/DataBase.java
+++ Just BeWeave Project/src/model/DataBase.java
@@ -1,9 +1,41 @@
 package model;
 
-public class DataBase {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-	public static void getUsers() {
-		//hello
+public class DataBase {
+	
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public static User[] getUsers() throws IOException  {
+		
+		ArrayList<User> users = new ArrayList<User>();
+		
+		try {
+			FileReader usrFile = new FileReader(new File("UserFile.csv"));
+			BufferedReader brFile = new BufferedReader(usrFile);
+			
+			String line = "";
+			String comma = ",";
+			
+			while ((line = brFile.readLine()) != null) {
+				String[] lineArr = line.split(comma);
+			}
+			
+			usrFile.close();
+			brFile.close();
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		
+		
+		return (User[]) users.toArray();
 	}
 	
 }
