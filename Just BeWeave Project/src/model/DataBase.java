f104bf3e7ee8abbc4ad45d29a97a939f01c19ad8
Peter <Peter@pc>
2017-03-08 20:44:44 -0800

    commit

--- Just BeWeave Project/src/model/DataBase.java
+++ Just BeWeave Project/src/model/DataBase.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 // NOTE THAT THE CSV WILL BE FORMATTED AS: EVENTNAME, USER1Name, USER1pswd, USER1boolean, UETC...
@@ -14,7 +15,53 @@ import java.util.List;
 public class DataBase {
 	
 	/**
-	 * This method gets the users from an event.
+	 * Grabs either one line or all the lines from a csv file.
+	 * @param theFile: the name of the csv file I want to check.
+	 * @param confVar: Identifying string if I'm looking for a specific line in the csv.
+	 * @param getAll: A boolean to see if I want all lines in the csv file.
+	 * @return A LinkedList of String arrays.
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	private static LinkedList<String[]> checkCSV(String theFile, String confVar, boolean getAll) 
+			throws IOException, FileNotFoundException {
+		
+		LinkedList<String[]> list = new LinkedList<String[]>();
+		
+		FileReader usrFile = new FileReader(new File(theFile));
+		BufferedReader brFile = new BufferedReader(usrFile);
+		
+		String line = "";
+		String comma = ",";
+		String[] lineArr;
+		
+		while ((line = brFile.readLine()) != null) {
+			
+			
+			//if (line.contains(confVar) && (lineArr = line.split(comma)).length > 1) {
+			if(getAll) {			
+				lineArr = line.split(comma); 
+				list.add(lineArr);
+				
+			} else if (line.contains(confVar)) {	
+				lineArr = line.split(comma); 	/* This line is repeated. If I'm looking
+				   								for a specific variable, I don't want to waste
+				   								time splitting a String unnecessarily. */
+				
+				list.add((String[]) lineArr.clone());
+				break;
+			}
+			
+		}
+		
+		usrFile.close();
+		brFile.close();
+		
+		return list;
+	}
+	
+	/**
+	 * This method gets the users for an event.
 	 * @param theEvent: The String event name.
 	 * @return The Array of Users in theEvent.
 	 * @throws IOException
@@ -24,6 +71,62 @@ public class DataBase {
 	public static List<User> getUsers(String theEvent) throws IOException {
 		
 		List<User> users = new ArrayList<User>();
+		
+		LinkedList<String[]> list = checkCSV("Events.csv", theEvent, false);
+		
+		String[] lineArr;
+		
+		if (list.size() > 0 && (lineArr = list.get(0)).length > 1) {
+			
+			for(int i = 1; i < lineArr.length; i++) {
+				//NonAdmin(String theUserName, String thePassword, boolean theAdmin)
+				String username = lineArr[i];
+				String pswd = lineArr[++i];
+				
+				boolean isAdmin = false;
+				if(lineArr[++i].contains("t")) {
+					isAdmin = true;
+				}
+				
+				NonAdmin user = new NonAdmin(username, pswd, isAdmin);
+				users.add(user);
+			}
+		}	
+		
+		return users;
+	}
+	
+	/**
+	 * Get a list of events.
+	 * @return A List of Events.
+	 * Post: a valid list of Events.
+	 */
+	public static List<Event> getEvents() {
+		List<Event> events = new ArrayList<Event>();
+		
+		return events;
+	}
+	
+	public static User verifyUser(String userName, String password) {
+		User u = null;
+		
+		return u;
+	}
+	
+	public static void saveEvent(Event theEvent) {
+		
+	}
+	
+	public static void saveUser(User theUser) {
+		
+	}
+	
+}
+
+
+/*
+//getUser sample
+List<User> users = new ArrayList<User>();
 		
 		boolean found = false;
 		
@@ -66,28 +169,5 @@ public class DataBase {
 		}
 		
 		return users;
-	}
-	
-	
-	
-	public static List<Event> getEvents() {
-		List<Event> events = new ArrayList<Event>();
-		
-		return events;
-	}
-	
-	public static User verifyUser(String userName, String password) {
-		User u = null;
-		
-		return u;
-	}
-	
-	public static void saveEvent(Event theEvent) {
-		
-	}
-	
-	public static void saveUser(User theUser) {
-		
-	}
-	
-}
+
+*/
