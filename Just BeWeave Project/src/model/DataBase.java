d166da966144e7e87d83119356cc2950d23bc89e
mmdahir <mmdahir@uw.edu>
2017-03-08 16:50:52 -0800

    Work on User class. -MD

--- Just BeWeave Project/src/model/DataBase.java
+++ Just BeWeave Project/src/model/DataBase.java
@@ -1,9 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class DataBase {
 
 	public static void getUsers() {
 		//hello
+	}
+	
+	public static ArrayList<Event> getEvents() {
+		
+		return null;
+	}
+	
+	public static User verifyUser(User user) {
+		
+		return null;
+	}
+	
+	public static void saveEvent(Event theEvent) {
+		
+	}
+	
+	public static void saveUser(User theUser) {
+		
 	}
 	
 }
