a8a93312b3ec982879da4bf65d579f2b0b996bbc
Abdullah <Abdullah@Fahmid>
2017-03-17 02:15:42 -0700

    Date test added

---
+++ Just BeWeave Project/src/model/DateTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DateTest {
+	
+	Date d1;
+	Date d2;
+
+	@Test
+	public void testEquals() {
+		d1 = new Date(12,12,12);
+		d2 = new Date(03,5,17);
+		
+		Date ds = new Date (3,5,17);
+		
+		assertEquals(ds, d2);
+		assertEquals(d1, new Date(12,12,12));
+	}
+
+	@Test
+	public void testToString() {
+		d1 = new Date(12,12,12);
+		d2 = new Date(03,5,17);
+		
+		String s1 = 12 + "," + 12 + "," + 12;
+		
+		assertEquals(s1, d1.toString());
+		assertEquals(3 + "," + 5 + "," + 17, d2.toString());
+	}
+
+}
