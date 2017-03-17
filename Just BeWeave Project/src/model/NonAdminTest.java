4bcfd4d0bc6af9af3b3b5628ef99b46d0bcc98a3
Abdullah <Abdullah@Fahmid>
2017-03-17 02:43:18 -0700

    Event class checks for duplicates before adding users

---
+++ Just BeWeave Project/src/model/NonAdminTest.java
@@ -0,0 +1,79 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NonAdminTest {
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNonAdmin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEvents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEventString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEventEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEventString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEventEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAdmin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsAbstractUser() {
+		fail("Not yet implemented");
+	}
+
+}
